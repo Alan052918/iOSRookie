f46ee8e2e2975cf929bb43b4276f7395f7570ca5
Alan Ai <aijunda29@gmail.com>
2021-05-16 10:13:58 +0800

    Emergent backup

---
+++ iOSRookie/ViewController+CategoryDemo.h
@@ -0,0 +1,19 @@
+//
+//  ViewController+CategoryDemo.h
+//  iOSRookie
+//
+//  Created by Alan on 2020/7/11.
+//  Copyright © 2020 aijunda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController (CategoryDemo)
+
+- (void)didSomething;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSRookie/ViewController+CategoryDemo.m
@@ -0,0 +1,17 @@
+//
+//  ViewController+CategoryDemo.m
+//  iOSRookie
+//
+//  Created by Alan on 2020/7/11.
+//  Copyright © 2020 aijunda. All rights reserved.
+//
+
+#import "ViewController+CategoryDemo.h"
+
+@implementation ViewController (CategoryDemo)
+
+- (void)didSomething {
+    NSLog(@"view controller did something");
+}
+
+@end
